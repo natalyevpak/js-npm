a3732f53e12e96769729de19348fb40d29860cd7
Nataly Evpak <natalyevpak@gmail.com>
2022-11-19 18:43:18 +0200

    added arrays js files

---
+++ arrays.js
@@ -0,0 +1,16 @@
+//Создание массива
+var myarray = ['Элемент1', 'Элемент2'];
+console.log('Init array');
+console.log(myarray);
+
+//Добавление элемента массива в конец
+var newLength = myarray.push('Элемент3');
+
+//Удаление последнего элемента
+var last = myarray.pop();
+
+//Удаление первого элемента массива
+var first = myarray.shift();
+console.log('Result array');
+console.log(myarray);
+
